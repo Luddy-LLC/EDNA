d7815e2e6fb53a96e8b8d20c06a89658e6ef37ff
NouveauReece <nouveaureece@protonmail.com>
2025-05-16 13:59:19 -0400

    Oops

--- auth/config.js
+++ auth/config.js
@@ -2,8 +2,8 @@ export const msalConfig = {
   auth: {
     clientId: "d7c6a2e3-5cef-4ca5-967a-3790bdc5e1dc",
     authority: "https://login.microsoftonline.com/1113be34-aed1-4d00-ab4b-cdd02510be91",
-    redirectUri: "http://127.0.0.1:3000/index.html",
-    postLogoutRedirectUri: "https://127.0.0.1:3000/logout.html"
+    redirectUri: "https://luddy-llc.github.io/EDNA/",
+    postLogoutRedirectUri: "https://luddy-llc.github.io/EDNA/logout"
   },
   cache: {
     cacheLocation: "localStorage",
